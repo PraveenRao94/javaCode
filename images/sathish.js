6f30f4bc294321234e1d08cae0b50a7204b8a6bc
Praveen Rao <Praveenrao9845@gmail.com>
2022-08-09 12:19:21 +0530

    java

---
+++ images/sathish.js
@@ -0,0 +1,12 @@
+class sati {
+  constructor(name, location) {
+    this.name = name;
+    this.location = location;
+  }
+  calcage() {
+    console.log(`nan hesaru ${this.name}`);
+  }
+}
+
+const S = new sati(satisha, lanka);
+console.log(calcage());
